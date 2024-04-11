9b1cc21b803bcf1aea401882cd894d1c330c6a3f
huzefa.taj <huzefa.taj@tiss.edu>
2024-04-11 18:15:41 +0530

    11-04-24 18:15

--- groceryapp/views.py
+++ groceryapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Category
 # Create your views here.
 def index(request):
-
-    return render(request, 'index.html')
+    category = Category.objects.all()
+    paras={'category':category}
+    return render(request, 'index.html',paras)
