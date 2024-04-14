4922cdab09a369073bcd8f513614370ecc8b52ec
huzefa.taj <huzefa.taj@tiss.edu>
2024-04-14 16:30:33 +0530

    14-04-24 16:30

--- groceryapp/admin.py
+++ groceryapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category,Product,Unit,Customer
 # Register your models here.
+admin.site.register(Customer)
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Unit)
--- groceryapp/models.py
+++ groceryapp/models.py
@@ -1,7 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    email = models.EmailField(unique=True)
+    password= models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.email
+    
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+
+class Unit(models.Model):
+    name = models.CharField(max_length=100,blank=True)
+    def __str__(self):
+         return self.name
+
+class Product(models.Model):
+    image_url = models.URLField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,blank=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+    qty = models.FloatField(default=0)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE,blank=True)
+
+   
+
+    
+
+    def __str__(self):
+        return self.title
+    
